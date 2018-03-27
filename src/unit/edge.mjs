4d2bd749d12c446c6e6b72ad9a3717598f9fbd2b
Ben Halverson <benhalverson33@gmail.com>
2018-03-26 22:14:53 -0700

    feat(edge.mjs): added setDistance, setWeight methods

--- src/unit/edge.mjs
+++ src/unit/edge.mjs
@@ -18,6 +18,54 @@ class Edge extends Unit {
     node.edges.push(this);
     return true;
   }
+
+  link(inputNode, outputNode, duplex) {
+    this.unlink();
+
+    this.inputNode = inputNode;
+    this.outputNode = outputNode;
+    this.duplex = !!duplex;
+
+    if(duplex) {
+      this._linkTo(inputNode, 0);
+      this._linkTo(outputNode, 0);
+      return this;
+    }
+
+    this._linkTo(inputNode, 1);
+    this._linkTo(outputNode, -1);
+    return this;
+  }
+
+  setDistance(v) {
+    this.distance = Math.abs(parseFloat(v) || 0);
+    return this;
+  }
+
+  setWeight(v) {
+    this.distance = 1 / Math.abs(parseFloat(v) || 0);
+    return this;
+  }
+
+  oppositeNode(node) {
+    if(this.inputNode === node) {
+      return this.outputNode;
+    } else if(this.outputNode === node) {
+      return this.inputNode;
+    }
+    return;
+  }
+
+  unlink() {
+    // let pos;
+    // let inode = this.inputNode;
+    // let onode = this.outputNode;
+  }
+
+  toJSON() {
+    return super.toJSON().concat(
+      [this.inputNode.uniqueID, this.outputNode.uniqueID, (this.duplex || 0, this.distance)]);
+  }
 }
 
 export default Edge;
